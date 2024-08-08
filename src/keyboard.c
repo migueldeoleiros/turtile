b1c1fb5b3659b30219fb0728926082daeeb60a6e
migueldeoleiros <migueldeoleiros@gmail.com>
2024-08-08 20:39:55 +0200

    feat: Read keybinds from config

--- src/keyboard.c
+++ src/keyboard.c
@@ -21,8 +21,12 @@
 */
 
 #include "keyboard.h"
+#include "config.h"
 #include "toplevel.h"
+
+#include "wlr/util/log.h"
 #include <stdlib.h>
+#include <unistd.h>
 #include <wlr/types/wlr_seat.h>
 #include <wlr/types/wlr_xdg_shell.h>
 
@@ -46,11 +50,7 @@ void keyboard_handle_modifiers(
 
 bool handle_keybinding(struct turtile_server *server, uint32_t modifiers,
 					   xkb_keysym_t sym) {
-    /*
-     * Here we handle compositor keybindings. This is when the compositor is
-     * processing keys, rather than passing them on to the client for its own
-     * processing.
-     */
+    turtile_keybind_t *keybind;
     switch (sym) {
     case XKB_KEY_Escape:
         wl_display_terminate(server->wl_display);
@@ -65,6 +65,14 @@ bool handle_keybinding(struct turtile_server *server, uint32_t modifiers,
         focus_toplevel(next_toplevel, next_toplevel->xdg_toplevel->base->surface);
         break;
     default:
+        wl_list_for_each(keybind, &config_get_instance()->keybinds, link) {
+            if (keybind->keys[0] == sym || keybind->keys[1] == sym || keybind->keys[2] == sym) {
+                wlr_log(WLR_INFO, "Executing command: %s", keybind->cmd);
+                if (fork() == 0)
+			        execl("/bin/sh", "/bin/sh", "-c", keybind->cmd, (void *)NULL);
+                return true;
+            }
+        }
         return false;
     }
     return true;
